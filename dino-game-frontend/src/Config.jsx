8c844762cc3a4fe8ea1696a822c3337b1f61306e
Farman <110935830+farman13@users.noreply.github.com>
2025-11-27 01:31:30 +0530

    Update Config.jsx

--- dino-game-frontend/src/Config.jsx
+++ dino-game-frontend/src/Config.jsx
@@ -21,7 +21,7 @@ import {
 
 const config = getDefaultConfig({
     appName: 'My RainbowKit App',
-    projectId: 'YOUR_PROJECT_ID',
+    projectId: '6dd15a3684137adf8eb5ed126f061236',
     chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
     ssr: true, // If your dApp uses server side rendering (SSR)
 });
